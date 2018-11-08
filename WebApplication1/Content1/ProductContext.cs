a23dc28fd0be55c03546c00b5ea04ab586877c1c
unknown <ducnnd00468@fpt.edu.vn>
2018-11-07 17:34:57 -0800

    update again

---
+++ WebApplication1/Content1/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Content1
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
